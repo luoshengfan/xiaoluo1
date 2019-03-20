9cbe406b3973abc1e9c455db50186c45d0eff04a
王帅 <2624318037.qq.com>
2019-03-20 20:46:16 +0800

    删除功能 date：20190320 负责人：王帅

--- article/views.py
+++ article/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import DetailView, ListView
+from .models import Article
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+class ArticleList(ListView):
+    model = Article
+    template_name = 'post_list.html'
+
+
+def article_delete(request, id):
+    Articles =Article.objects.get(id=id)
+    Articles.delete()
+    return redirect('post_list')
+
+
+class  ArticleDetail(DetailView):
+    model = Article
+    template_name = 'post_detail.html'
+
